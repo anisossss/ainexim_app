220caae88e83245c50db54981c13cc8ddcadb5c2
anis <anis.khalef98@gmail.com>
2024-03-05 21:58:22 +0100

    latest

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,6 @@
 export const CONSTANTS = {
   // API_URL: "http://localhost:4402/api",
   API_URL: "https://api.ainexim.com/api",
-  // SOCKET_URL: "https://api.ainexim.com",
+  // SOCKET_URL: "http://localhost:4402",
   SOCKET_URL: "https://api.ainexim.com",
 };
